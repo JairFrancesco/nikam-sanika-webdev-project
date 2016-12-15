666433134c45db4c103bef98e73bb0b3b27c72df
Sanika  Nikam <sanikanikam@Sanikas-MacBook-Pro.local>
2016-12-15 12:05:20 -0500

    added deployment changes

--- project/model/models.server.js
+++ project/model/models.server.js
@@ -1,6 +1,7 @@
 module.exports = function(){
 	var mongoose = require ('mongoose');
-	mongoose.connect('mongodb://localhost/foodster-fall-2016');
+	//mongoose.connect('mongodb://localhost/foodster-fall-2016');
+	mongoose.connect('mongodb://sanikanikam:sanikanikam@ec2-54-211-101-168.compute-1.amazonaws.com:27017/dummyDB');
 	var userModel = require("./user/user.model.server")();
 	var reviewModel = require("./review/review.model.server")();
 	var restaurantModel = require("./restaurant/restaurant.model.server")();
